11310219f10801eafbc54f5867f81a342c25bf07
Jiangwen <idleuncle@gmail.com>
2016-11-03 23:39:48 +0800

    Remove unused code.

--- tools/ofdviewer.cxx
+++ tools/ofdviewer.cxx
@@ -154,6 +154,7 @@ void SDLApp::ToggleFullScreen(){
     }
 }
 
+// ======== SDLApp::Execute() ========
 void SDLApp::Execute(){
     bool done = false;
     while ( !done ){
@@ -232,6 +233,7 @@ void SDLApp::Execute(){
     };
 }
 
+// ======== SDLApp::RenderScreen() ========
 void SDLApp::RenderScreen() const {
     // Render(imageSurface)
     //SDL_Texture *backgroundTexture = SDL_CreateTexture(m_screenRenderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, m_screenWidth, m_screenHeight);
@@ -262,10 +264,12 @@ void SDLApp::OnEvent(SDL_Event event, bool &done){
 void SDLApp::OnRender(cairo_surface_t *surface){
 }
 
+// ======== SDLApp::RefreshScreen() ========
 void SDLApp::RefreshScreen(uint64_t color) const{
     SDL_FillRect(m_imageSurface, NULL, color);
 }
 
+// ======== SDLApp::Loop() ========
 void SDLApp::Loop(){
     RefreshScreen(0xFFFFFF);
 
@@ -280,6 +284,7 @@ void SDLApp::Loop(){
     cairo_surface_destroy(cairoSurface);
 }
 
+// **************** class MySDLApp ****************
 class MySDLApp : public SDLApp {
 public:
     MySDLApp(const std::string &title, double screenWidth, double screenHeight, int screenBPP, OFDDocumentPtr document);
@@ -293,11 +298,14 @@ public:
 
 }; // class MySDLApp
 
+
+// ======== MySDLApp::MySDLApp() ========
 MySDLApp::MySDLApp(const std::string &title, double screenWidth, double screenHeight, int screenBPP, OFDDocumentPtr document)
     : SDLApp(title, screenWidth, screenHeight, screenBPP),
         m_document(document), m_pageIndex(0){
 }
 
+// ======== MySDLApp::~MySDLApp() ========
 MySDLApp::~MySDLApp(){
 }
 
@@ -352,9 +360,6 @@ void MySDLApp::OnRender(cairo_surface_t *surface){
     }
 }
 
-
-bool fullScreen = false;
-
 // -------- loadTexture() --------
 SDL_Texture *loadTexture(SDL_Renderer *renderer, const std::string &image_file){
     SDL_Texture *texture = nullptr;
@@ -407,253 +412,6 @@ int main(int argc, char *argv[]){
     package.Close();
 
 
-
-    LOG(INFO) << "Done.";
-
-    return 0;
-}
-
-//int main(){
-int main_old(int argc, char *argv[]){
-
-    TIMED_FUNC(timerMain);
-
-    Logger::Initialize(argc, argv);
-
-    LOG(INFO) << "Start " << argv[0];
-
-
-    OFDPackage package;
-    OFDDocumentPtr document = nullptr;
-    OFDPagePtr currentPage = nullptr;
-    size_t totalPages = 0;
-    size_t showPage = 0;
-    if ( package.Open(argv[1]) ){
-        document = OFDDocumentPtr(package.GetOFDDocument()); 
-        //OFDDocumentPtr document = package.GetOFDDocument(); 
-
-        LOG(DEBUG) << document->String();
-
-        size_t n_pages = document->GetPagesCount();
-        totalPages = n_pages;
-        LOG(INFO) << "Loading " << n_pages << " pages.";
-
-        //for ( size_t i = 0 ; i < n_pages ; i++ ){
-            //TIMED_SCOPE(timerDrawPage, "Draw Page");
-
-            //OFDPagePtr page = document->GetOFDPage(i);
-            ////page = document->GetOFDPage(i);
-            //page->Open();
-
-            //VLOG(3) << page->String(); 
-            //LOG(INFO) << page->GetText();
-
-            ////std::stringstream ss;
-            ////ss << "Page" << (i + 1) << ".png";
-            ////std::string png_filename = ss.str();
-            ////page->RenderToPNGFile(png_filename);
-
-            ////page->Close();
-        //}
-        currentPage = document->GetOFDPage(0);
-        currentPage->Open();
-
-    } else {
-        LOG(ERROR) << "package.Open() failed. " << argv[1];
-        return -1;
-    }
-
-
-    if ( SDL_Init(SDL_INIT_VIDEO) < 0 ){
-        LOG(ERROR) << "Failed to initialize SDL. " << SDL_GetError();
-        return -1;
-    }
-
-    if ( !SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1") ){
-        LOG(ERROR) << "SDL_SetHint() failed. " << SDL_GetError();
-        return -1;
-    }
-
-    int screenWidth = 794;
-    int screenHeight = 1122;
-    int screenFlags = SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE; // SDL_WINDOW_BORDERLESS
-    int screenBPP = 32;
-    SDL_Window *mainWindow = SDL_CreateWindow("OFD Viewer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screenWidth, screenHeight, screenFlags);
-    if ( mainWindow == nullptr ){
-        LOG(ERROR) << "SDL_CreateWindow() failed. " << SDL_GetError();
-        return -1;
-    }
-
-    //SDL_Surface *primarySurface = SDL_GetWindowSurface(mainWindow);
-    //cairo_surface_t *cairoSurface = create_cairo_surface_from_sdl_surface(primarySurface);
-    //if ( cairoSurface == nullptr ){
-        //LOG(ERROR) << "create_cairo_surface_from_sdl_surface() failed.";
-        //return -1;
-    //}
-
-    SDL_Renderer *screenRenderer = SDL_CreateRenderer(mainWindow, -1, SDL_RENDERER_ACCELERATED);// | SDL_RENDERER_PRESENTVSYNC);
-    if ( screenRenderer == nullptr ){
-        LOG(ERROR) << "SDL_CreateRenderer() failed. " << SDL_GetError();
-        return -1;
-    }
-
-    if ( !(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) ){
-        LOG(ERROR) << "IMG_Init() failed. " << SDL_GetError();
-        return -1;
-    }
-
-
-    SDL_Surface *imageSurface = create_image_surface(screenWidth, screenHeight, screenBPP);
-
-    bool done = false;
-    while ( !done ){
-        SDL_Event event;
-        while ( SDL_PollEvent(&event) != 0 ){
-            if ( event.type == SDL_QUIT ){
-                done = true;
-            }
-            // OnEvent(event);
-            switch (event.type) {
-            //case SDL_TEXTINPUT:
-                //break;
-            case SDL_KEYDOWN:
-                if (event.key.keysym.sym == SDLK_ESCAPE){
-                    done = true;
-                } else if (event.key.keysym.sym == SDLK_q){
-                    //SDL_GetModState() & KMOD_CTRL
-                    //inputText = SDL_GetClipboardText();
-                    done = true;
-                } else if (event.key.keysym.sym == SDLK_UP) {
-                    if ( showPage == 0 ){
-                        showPage = totalPages - 1;
-                    } else {
-                        showPage--;
-                    }
-                    currentPage = document->GetOFDPage(showPage);
-                    currentPage->Open();
-                    break;
-                } else if (event.key.keysym.sym == SDLK_DOWN) {
-                    if ( showPage < totalPages - 1 ){
-                        showPage++;
-                    } else {
-                        showPage = 0;
-                    }
-                    currentPage = document->GetOFDPage(showPage);
-                    currentPage->Open();
-                    break;
-                } else if (event.key.keysym.sym == SDLK_RETURN) {
-                    if (fullScreen){
-                        SDL_SetWindowFullscreen(mainWindow, SDL_FALSE);
-                        fullScreen = false;
-                    } else {
-                        SDL_SetWindowFullscreen(mainWindow, SDL_TRUE);
-                        fullScreen = true;
-                    }
-                }
-                break;
-            case SDL_MOUSEMOTION:
-            case SDL_MOUSEBUTTONDOWN:
-            case SDL_MOUSEBUTTONUP:
-                {
-                    int mouseX, mouseY;
-                    SDL_GetMouseState(&mouseX, &mouseY);
-                }
-                break;
-            case SDL_WINDOWEVENT:
-                switch ( event.window.event ){
-                case SDL_WINDOWEVENT_SIZE_CHANGED:
-                    screenWidth = event.window.data1;
-                    screenHeight = event.window.data2;
-
-                    //SDL_DestroyRenderer(screenRenderer);
-                    //screenRenderer = SDL_CreateRenderer(mainWindow, -1, SDL_RENDERER_ACCELERATED); // | SDL_RENDERER_PRESENTVSYNC);
-                    //if ( screenRenderer == nullptr ){
-                        //LOG(ERROR) << "SDL_CreateRenderer() failed. " << SDL_GetError();
-                        //return -1;
-                    //}
-
-                    SDL_FreeSurface(imageSurface);
-                    imageSurface = create_image_surface(screenWidth, screenHeight, screenBPP);
-                    SDL_RenderPresent(screenRenderer);
-                    break;
-                case SDL_WINDOWEVENT_EXPOSED:
-                    SDL_RenderPresent(screenRenderer);
-                    break;
-
-                case SDL_WINDOWEVENT_ENTER:
-                    break;
-                case SDL_WINDOWEVENT_LEAVE:
-                    break;
-                case SDL_WINDOWEVENT_FOCUS_GAINED:
-                    break;
-                case SDL_WINDOWEVENT_FOCUS_LOST:
-                    break;
-                case SDL_WINDOWEVENT_MINIMIZED:
-                    break;
-                case SDL_WINDOWEVENT_MAXIMIZED:
-                    break;
-                case SDL_WINDOWEVENT_RESTORED:
-                    break;
-                };
-                break;
-            };
-        };
-
-        // Loop()
-        SDL_FillRect(imageSurface, NULL, 0xFFFFFF);
-
-
-        cairo_surface_t *cairoSurface = create_cairo_surface_from_sdl_surface(imageSurface);
-        if ( cairoSurface == nullptr ){
-            LOG(ERROR) << "create_cairo_surface_from_sdl_surface() failed.";
-            return -1;
-        }
-
-        if ( currentPage != nullptr ){
-            currentPage->Render(cairoSurface);
-        }
-
-        // Render(imageSurface)
-        //SDL_Texture *backgroundTexture = SDL_CreateTexture(screenRenderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, screenWidth, screenHeight);
-        //if ( backgroundTexture == nullptr ){
-            //LOG(ERROR) << "SDL_CreateTexture() failed. " << SDL_GetError();
-            //return -1;
-        //}
-        //SDL_UpdateTexture(backgroundTexture, NULL, imageSurface->pixels, imageSurface->pitch);
-
-        SDL_Texture *backgroundTexture = SDL_CreateTextureFromSurface(screenRenderer, imageSurface);
-
-        // Clear screen
-        SDL_SetRenderDrawColor(screenRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-        SDL_RenderClear(screenRenderer);
-        // Render texture to screen
-        SDL_Rect destRect{0, 0, screenWidth, screenHeight};
-        SDL_RenderCopy(screenRenderer, backgroundTexture, NULL, &destRect);
-
-        // Update screen
-        SDL_RenderPresent(screenRenderer);
-
-        SDL_DestroyTexture(backgroundTexture);
-
-        SDL_Delay(1); // Breath
-    };
-
-    // Cleanup()
-    SDL_FreeSurface(imageSurface);
-    if ( screenRenderer != nullptr ){
-        SDL_DestroyRenderer(screenRenderer);
-        screenRenderer = nullptr;
-    }
-
-    if ( mainWindow != nullptr ){
-        SDL_DestroyWindow(mainWindow);
-        mainWindow = nullptr;
-    }
-
-    IMG_Quit();
-    SDL_Quit();
-
-    package.Close();
 
     LOG(INFO) << "Done.";
 
